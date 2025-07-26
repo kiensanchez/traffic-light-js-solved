fe488879e58fd31dd337980a8ba7cc0320ea255f
kiensanchez <kiensanchez@yahoo.com>
2025-07-25 20:40:02 -0500

    initial commit

--- src/ClassTrafficLight.jsx
+++ src/ClassTrafficLight.jsx
@@ -1,17 +1,31 @@
 import { Component } from "react";
 
 export class ClassTrafficLight extends Component {
+  state = {
+    index: 0
+  }
+
+  btnNext = () => {
+    if(this.state.index < 2) {
+      this.setState({index: this.state.index + 1})
+    } else this.setState({index: 0})
+    console.log(this.state.index)
+  }
+
   render() {
+    const lightColors = ['red', 'green', 'yellow']
+    const trafficLight = lightColors[this.state.index]
+
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
           {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div className={`circle ${trafficLight === 'red' ? 'red' : 'black'}`}></div>
+          <div className={`circle ${trafficLight === 'yellow' ? 'yellow' : 'black'}`}></div>
+          <div className={`circle ${trafficLight === 'green' ? 'green' : 'black'}`}></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button onClick={this.btnNext} className="next-state-button">Next State</button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.jsx
+++ src/FunctionalTrafficLight.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
+
 export const FunctionalTrafficLight = () => {
+  const [index, setIndex] = useState(0)
+  const lightColors = ['red', 'green', 'yellow']
+  const trafficLight = lightColors[index]
+
+  const btnNext = () => {
+    if(index < 2) {
+      setIndex(index + 1)
+    } else setIndex(0)
+  }
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${trafficLight === 'red' ? 'red' : 'black'}`}></div>
+        <div className={`circle ${trafficLight === 'yellow' ? 'yellow' : 'black'}`}></div>
+        <div className={`circle ${trafficLight === 'green' ? 'green' : 'black'}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button onClick={btnNext} className="next-state-button">Next State</button>
     </div>
   );
 };
